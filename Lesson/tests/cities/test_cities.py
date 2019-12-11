d0bd51f7a8327e18dc1dd3d2a9c7a183753d0caf
nlavoine <nlavoine.dev@gmail.com>
2019-12-11 11:57:30 +0100

    REFACTORED TEST FUNCTION SIGNATURE
    Renamed function name to be more explicit and match the tested function name

--- Lesson/tests/cities/test_cities.py
+++ Lesson/tests/cities/test_cities.py
@@ -3,7 +3,7 @@ from apps.cities.cities import *
 from settings import CITIES_CSV_FILE
 
 
-def test_cities():
+def test_get_data_frame_from_csv():
     datas = get_data_frame_from_csv('../../' + CITIES_CSV_FILE)
     sample = open("golden-master.txt", encoding='utf-8')
 
