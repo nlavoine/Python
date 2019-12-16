6b29334306b1bffcba50dcfa5d271d7c727b05ba
nlavoine <nlavoine.dev@gmail.com>
2019-12-16 10:56:13 +0100

    Added tests for schools
    Tests failed. Commit before per-prog

--- Lesson/apps/schools/schools.py
+++ Lesson/apps/schools/schools.py
@@ -12,7 +12,10 @@ def get_datas_from_schools_csv(file_path):
 
 
 def merge_row_by_insee_code(data_frame):
-    data_frame_grouped = data_frame.groupby(['Code commune'])
-    # print(data_frame.head(-1))
-    print(data_frame_grouped)
+    data_frame_grouped = data_frame.groupby(['Code commune']).sum()
+    # solution = {'Index': [3, 1, 2], 'Villes': ['Lyon', 'Lyon', 'Marseille'], 'Population': [1, 2, 3], 'datas': [5, 10, 15]}
+    # solution_frame = pd.DataFrame(data=solution, index=None)
+    # solution_frame_grouped = solution_frame.groupby(['Villes']).sum()
+    # print(data_frame_grouped.mean())
+    return data_frame_grouped
 
--- Lesson/run.py
+++ Lesson/run.py
@@ -8,7 +8,8 @@ from settings import SCHOOLS_CSV_FILE
 def main():
     # data_frame_cities = get_data_frame_from_cities_csv(CITIES_CSV_FILE)
     data_frame_schools = get_datas_from_schools_csv(SCHOOLS_CSV_FILE)
-    data_frame_schools = merge_row_by_insee_code(data_frame_schools)
+    data_frame_schools_merged = merge_row_by_insee_code(data_frame_schools)
+    print(data_frame_schools_merged)
     # Get, Format, Show
 
 
